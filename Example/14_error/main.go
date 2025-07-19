2d0763345dbd2a2b38073b1dd921ec8d43e6a5e2
parth469 <parthmain2001@gmail.com>
2025-07-19 12:12:57 +0530

    Add error handling and concurrency examples in Go. Introduced custom error types, implemented a function for error generation, and created a worker pool pattern with job processing and result handling. Added examples for channels, context cancellation, and various concurrency patterns.

--- Example/14_error/main.go
+++ Example/14_error/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+var NotFound error = errors.New("No result Found 0")
+var NotFound1 error = errors.New("No result Found 1 ")
+
+// type Error interface {
+// 	Error() string
+// }
+
+// Error.New
+// fmt.ErrorString
+
+func getError(returnValue int) (int, error) {
+	if returnValue == 0 {
+		return 0, fmt.Errorf("Insie of Erorr")
+	}
+	return returnValue, nil
+}
+func main() {
+	value, err := getError(0)
+
+	if err != nil {
+		fmt.Println(errors.Is(err, NotFound1))
+		fmt.Println(errors.Is(err, NotFound))
+		// fmt.Println(errors.As(NotFound, NotFound1)) it must be inerface
+
+		errJoin := errors.Join(NotFound, err, NotFound1)
+		fmt.Println(errJoin)
+		return
+	}
+
+	fmt.Println(value)
 }
